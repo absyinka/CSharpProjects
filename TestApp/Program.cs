7a7e381c9ee7b121126e11a568bac10bcce49b3f
absyinka <absyinka@gmail.com>
2022-07-03 10:17:02 +0100

    Ab intio

---
+++ TestApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        int count = 0;
+        static void Main(string[] args)
+        {
+            /*int result = MyMethod();
+            Console.WriteLine(result);
+
+            double result2 = MyMethod2(55.8, 44);
+            Console.WriteLine(result2);
+
+            double result3 = MyMethod2(123, 98.02);
+            Console.WriteLine(result3);
+
+            Console.Write("Enter your username: ");
+            string userName = Console.ReadLine();
+            string output = MyMethod3(userName);
+            Console.WriteLine(output);*/
+
+            Console.Write("Enter the number to get the mutiplication for: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("How many times? ");
+            int times = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            PrintMultiplicationTable(number, times);
+        }
+
+        static int MyMethod()
+        {
+            return 2 + 2;
+        }
+
+        static double MyMethod2(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        static string MyMethod3(string userName)
+        {
+            return $"Hello {userName}, welcome to MGQS.";
+        }
+
+        static void PrintMultiplicationTable(int number, int times)
+        {
+            for(int i = 1; i <= times; i++)
+            {
+                Console.WriteLine($"{number} * {i} = {number * i}");
+            }
+        }
+    }
+}
---
+++ TestApp/Tester.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class Tester
+    {
+        public void TestingTesting()
+        {
+            Console.Write("Enter coefficient of a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter coefficient of b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter coefficient of c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            double discriminant = b * b - 4 * a * c;
+            double x1;
+            double x2;
+
+            if (discriminant == 0)
+            {
+                x1 = -b / 4 * a * c;
+                Console.WriteLine($"the real root is {x1}");
+            }
+            else if (discriminant > 0)
+            {
+                x1 = -b - Math.Sqrt(discriminant) / 2 * a;
+                x2 = -b + Math.Sqrt(discriminant) / 2 * a;
+                Console.WriteLine($"the roots of the equations are {x1} and {x2}");
+            }
+            else
+            {
+                Console.WriteLine("it has no reall root");
+            }
+        }
+    }
+}
